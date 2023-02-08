beb5bda3f3e819dccbeb92898c17d02564aa63a7
N.I.K <85172286+masquerade28@users.noreply.github.com>
2023-02-08 09:26:30 +0530

    Update Young Physicist.py

--- Young Physicist.py
+++ Young Physicist.py
@@ -1,7 +1,16 @@
+# Taking Input For Number Of Forces Which Are Acting Upon The Object
 forces = int(input())
+
+# Creating an empty list to store the forces
 L = []
+
+# Appending the co-ordinates of the forces to the list L
 for i in range(forces):
     L.append([int(i) for i in input().split()])
+
+# Checking whether the sum of all the forces is zero or not
+# If Yes, the object is in equilibrium
+# Otherwise, it is not
 
 if L[0][0] + L[1][0] + L[2][0] == 0 and L[0][1] + L[1][1] + L[2][1] == 0 and L[0][2] + L[1][2] + L[2][2] == 0 :
     print("YES")
